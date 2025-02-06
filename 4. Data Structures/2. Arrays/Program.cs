5070a73343b08d16e8c9d819e56af5f33bcf1062
jldpDev <joseluisdp297@gmail.com>
2025-02-06 16:42:14 +0000

    Add array code

---
+++ 4. Data Structures/2. Arrays/Program.cs
@@ -0,0 +1,15 @@
+﻿// Create an array of strings
+string[] fruits = {"Apple", "Banana", "Orange", "Mango"};
+
+foreach(string fruit in fruits)
+{
+    Console.WriteLine(fruit);
+}
+
+// Create an array of numbers
+int[] numbers = new int[] {7, 1, 2, 5};
+
+foreach(int number in numbers)
+{
+    Console.WriteLine(number);
+}
