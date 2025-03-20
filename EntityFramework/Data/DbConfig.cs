800a71f67100b2f2436e76afb5e1ee7a59227129
jldpDev <joseluisdp297@gmail.com>
2025-03-20 20:30:59 +0000

    Create configuracion for EF Core

---
+++ EntityFramework/Data/DbConfig.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using EntityFramework.Data;
+using System.IO;
+
+
+namespace EntityFramework.Data
+{
+    public static class DbConfig
+    {
+        public static void ConfigureDatabase(IServiceCollection services)
+        {
+            // Load configuration from appsettings.json
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            // Retrieve the configuration string  
+            var configString = config.GetConnectionString("DefaultConnection"); 
+
+            // Register the DbContext in the dependencies container.  
+            services.AddDbContext<EmployeeContext>(options => 
+                options.UseNpgsql(configString));
+        }
+    }
+}
