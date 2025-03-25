937e1c3d3ad63c8f945a7521a40fe2ab54f0ef4f
JoseDev <132334488+devjldp@users.noreply.github.com>
2025-03-25 10:08:38 +0000

    Add OnModelCreating Method to EmployeeContext class

--- EntityFramework/Data/EmployeesDbContext.cs
+++ EntityFramework/Data/EmployeesDbContext.cs
@@ -1,9 +1,17 @@
 namespace EntityFramework.Data{
     public class EmployeeContext : DbContext
     {
+        // This property maps to the 'Employees' table in the database
         public DbSet<Employee> Employees { get; set;}
 
         // constructor:
         public EmployeeContext(DbContextOptions<EmployeeContext> options) : base(options){ }
+
+        // Override OnModelCreating to configure the entity relationships if needed
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Here you can define any additional configuration if required
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
