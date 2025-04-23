58923565ee020d9894c07b4dc654c8cbfc3b763f
jldpDev <joseluisdp297@gmail.com>
2025-04-23 18:10:24 +0100

    Cooment methods in interface

--- 12. Entity Framework/Repositories/EmployeeRepository.cs
+++ 12. Entity Framework/Repositories/EmployeeRepository.cs
@@ -2,22 +2,29 @@ using System.Collections.Generic;
 using System.Linq;
 using Employees.Models;
 using Employees.DataAnnotations;
+using Employees.Repositories;
 
 namespace Employees.Repositories
 {
-    public class EmployeeRepository : IRepository
+    public class EmployeeRepository : IEmployeeRepository
     {
-         private readonly EmployeesDbContext _context;
+        private readonly EmployeesDbContext _context; // Private field to hold the reference to the database context.
 
-        public EmployeeRepository(EmployeesDbContext context)
+        public EmployeeRepository(EmployeesDbContext context) // Constructor that receives the database context via dependency injection.
         {
             _context = context;
         }
 
+        // Method to add a new employee to the database
         public void AddEmployee(Employee employee)
         {
-            _context.Employees.Add(employee);
-            _context.SaveChanges();
+            _context.Employees.Add(employee); // Adds the new employee to the Employees table (tracked by EF Core)
+            _context.SaveChanges(); // Saves the changes to the database (executes the SQL INSERT)
+        }
+
+        public IEnumerable<Employee> GetAllEmployees()
+        {
+            return _context.Employees.ToList();
         }
 
     }
--- 12. Entity Framework/Repositories/IEmployeeRepository.cs
+++ 12. Entity Framework/Repositories/IEmployeeRepository.cs
@@ -6,10 +6,10 @@ namespace Employees.Repositories
     // Interface that defines the contract for employee-related data operations
     public interface IEmployeeRepository
     {
-        IEnumerable<Employee>GetAllEmployees(); // Returns a list of all employees in the database
-        Employee GetEmployeeById(int id); // Returns a single employee based on the given ID
+        IEnumerable<Employee> GetAllEmployees(); // Returns a list of all employees in the database
+        // Employee GetEmployeeById(int id); // Returns a single employee based on the given ID
         void AddEmployee(Employee employee); // Adds a new employee to the database
-        void UpdateEmployee(Employee employee); // Updates the data of an existing employee
-        void DeleteEmployee(int id); // Deletes an employee by their ID
+        // void UpdateEmployee(Employee employee); // Updates the data of an existing employee
+        //void DeleteEmployee(int id); // Deletes an employee by their ID
     }
 }
